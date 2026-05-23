e6107981d986892b08ea5665e126919f77b9891c
𝗦𝗢𝗨𝗠𝗘𝗦𝗪𝗔𝗥 神 <132776933+sowwmmm@users.noreply.github.com>
2026-05-23 23:28:28 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ struct Vertex {
     color: [f32; 3],
 }
 
-unsafe impl bytemuck::Zeroable for Vertex {}
-unsafe impl bytemuck::Pod for Vertex {}
-
 const VERTICES: &[Vertex] = &[
     Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
     Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
